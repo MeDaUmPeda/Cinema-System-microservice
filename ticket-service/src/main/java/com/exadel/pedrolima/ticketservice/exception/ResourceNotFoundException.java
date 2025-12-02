27922a257951578a51cacfb22879761e85b93e98
LordeP26 <ped.li2608@gmail.com>
2025-12-02 11:58:20 -0300

    Remove all microservices from master

--- ticket-service/src/main/java/com/exadel/pedrolima/ticketservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.exadel.pedrolima.ticketservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- user-service/src/main/java/com/exadel/pedrolima/userservice/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.exadel.pedrolima.userservice.exception;
-
-public class BadRequestException extends RuntimeException {
-  public BadRequestException(String message) {
-    super(message);
-  }
-}
